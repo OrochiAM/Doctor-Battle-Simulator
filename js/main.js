b0753fdbccf25994550992148616cceac11a86a2
Aleksa Milic <baba12milic@gmail.com>
2026-01-07 00:09:46 +0100

    Gulp sound.

--- js/main.js
+++ js/main.js
@@ -5,15 +5,18 @@ let counter = 1;
 let score = 0;
 
 apple.addEventListener('click', () => {
-  let audio = new Audio('audio/chomp.mp3');
-  audio.play();
-
   apple.src = `images/a${counter % 8}.png`;
 
   if (counter % 8 == 0) {
     score++;
     scoreP.innerHTML = `${score} 🍎`;
+
+    let gulp = new Audio('audio/gulp.mp3');
+    gulp.play();
   }
 
   counter++;
+
+  let chomp = new Audio('audio/chomp.mp3');
+  chomp.play();
 });
